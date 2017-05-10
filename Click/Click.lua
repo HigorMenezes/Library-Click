8f80e03b9ea3ca8b4d447b3d62398474da14c98e
higormenezes <higor_menezes96@hotmail.com.br>
2017-05-10 13:35:03 -0400

    Cursor Hand
    Ao passar o mouse em cima do botão o cursor vira o hand cursor

--- Click/Click.lua
+++ Click/Click.lua
@@ -3,48 +3,87 @@ local Click = {}
 local BASE = (...):match('(.-)[^%.]+$')
 
 local fontDefault = BASE .. "/font/FrancoisOne-Regular.ttf"
+local cursorHand = love.mouse.getSystemCursor("hand")
 
-local buttons = {}
+local rectangleButtons = {}
 
-function Click:newButton(paramLabel, paramPos, paramColor)
-	local button = {}
+function Click:newRectangleButton(param)
+	local btn = {}
 
-	button.label = {}
-	button.label.text = paramLabel.text or ""
-	button.label.font = paramLabel.font or fontDefault
-	button.label.size = paramLabel.size or 16
+	btn.Class = param.Class or "none"
 
-	button.pos = {}
-	button.pos.x = paramPos.x or 0
-	button.pos.y = paramPos.y or 0
-	button.pos.width = paramPos.width or 400
-	button.pos.height = paramPos.height or 100
-	button.pos.radius = math.min(paramPos.radius or 0, button.pos.height/2)
+	btn.label = {}
+	btn.label.text = param.label.text or "btn"
+	btn.label.font = param.label.font or fontDefault
+	btn.label.size = param.label.size or 16
 
-	button.color = {}
-	button.color.r = paramColor.r or 255
-	button.color.g = paramColor.g or 255
-	button.color.b = paramColor.b or 255
-	button.color.a = paramColor.a or 255
+	btn.button = {}
+	btn.button.x = param.button.x or 0
+	btn.button.y = param.button.y or 0
+	btn.button.width = param.button.width or 400
+	btn.button.height = param.button.height or 100
+	btn.button.radius = math.min(param.button.radius or 0, btn.button.height/2)
 
-	table.insert(buttons, button)
+	btn.color = {}
+	btn.color.r = param.color.r or 255
+	btn.color.g = param.color.g or 255
+	btn.color.b = param.color.b or 255
+	btn.color.a = param.color.a or 255
+
+	table.insert(rectangleButtons, btn)
 end
 
 function Click:draw()
-	for i=1,#buttons do
-		love.graphics.setColor(buttons[i].color.r, buttons[i].color.g, buttons[i].color.b, buttons[i].color.a)
-		love.graphics.rectangle("fill", buttons[i].pos.x, buttons[i].pos.y, buttons[i].pos.width, buttons[i].pos.height, buttons[i].pos.radius)
+	for i=1,#rectangleButtons do
+		local r = rectangleButtons[i]
+		love.graphics.setColor(r.color.r, r.color.g, r.color.b, r.color.a)
+		love.graphics.rectangle("fill", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
 
 		love.graphics.setColor(0, 0, 0, 255)
-		love.graphics.rectangle("line", buttons[i].pos.x, buttons[i].pos.y, buttons[i].pos.width, buttons[i].pos.height, buttons[i].pos.radius)
+		love.graphics.rectangle("line", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
 
 		love.graphics.setColor(0, 0, 0, 255)
-		love.graphics.setFont(love.graphics.newFont(buttons[i].label.font, buttons[i].label.size))
-		love.graphics.printf(buttons[i].label.text, buttons[i].pos.x, buttons[i].pos.y, buttons[i].pos.width, "center")
+		love.graphics.setFont(love.graphics.newFont(r.label.font, r.label.size))
+		love.graphics.printf(r.label.text, r.button.x, r.button.y, r.button.width, "center")
+		--love.graphics.newText()
 	end
 end
 
 function Click:update(dt)
+
+	if inside() then
+		love.mouse.setCursor(cursorHand)
+	else
+		love.mouse.setCursor()
+	end
+
+end
+
+function inside()
+	x = love.mouse.getX()
+	y = love.mouse.getY()
+
+	for i=1,#rectangleButtons do
+		local r = rectangleButtons[i].button
+		if x > r.x and x < r.x + r.width 
+			and y > r.y and y < r.y + r.height then
+			if r.radius > 0 
+				and (x > r.x + r.radius and x < r.x + r.width - r.radius) 
+				or (y > r.y + r.radius and y < r.y + r.height - r.radius) 
+				or ((x - (r.x + r.radius))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
+				or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
+				or ((x - (r.x + r.radius))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
+				or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
+				then
+				return true
+			else
+				return false
+			end
+			
+		else
+			return false
+		end
+	end
 end
 
 return Click
--- main.lua
+++ main.lua
@@ -2,11 +2,16 @@ click = require("Click")
 
 function love.load()
 	love.graphics.setBackgroundColor(170, 170, 170)
-	click:newButton({text = "BUTTON", size = 70, font = "font/FrancoisOne-Regular.ttf"}, {x = 200, y = 200, width = 400, height = 100, radius = 15}, {r = 255, g = 255, b = 255, a = 255})
+	click:newRectangleButton({
+		ID = "play",
+		label = {text = "PLAY", size = 70, font = "font/FrancoisOne-Regular.ttf"}, 
+		button = {x = 200, y = 200, width = 400, height = 100, radius =  50}, 
+		color = {r = 255, g = 255, b = 255, a = 255}
+		})
 end
 
 function love.update(dt)
-
+	click:update(dt)
 end
 
 function love.draw()
