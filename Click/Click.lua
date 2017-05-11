94e531bdaf49a924e3d8292bcadb6eba9aade306
higormenezes <higor_menezes96@hotmail.com.br>
2017-05-10 20:03:50 -0400

    Border
    Acrescentado a escolha de borda

--- Click/Click.lua
+++ Click/Click.lua
@@ -4,6 +4,8 @@ local BASE = (...):match('(.-)[^%.]+$')
 
 local fontDefault = BASE .. "/font/FrancoisOne-Regular.ttf"
 local cursorHand = love.mouse.getSystemCursor("hand")
+local flag = true
+local hover = {false, "none", 0}
 
 local rectangleButtons = {}
 
@@ -16,6 +18,11 @@ function Click:newRectangleButton(param)
 	btn.label.text = param.label.text or "btn"
 	btn.label.font = param.label.font or fontDefault
 	btn.label.size = param.label.size or 16
+	btn.label.color = {}
+	btn.label.color.r = param.label.color.r or 0
+	btn.label.color.g = param.label.color.g or 0
+	btn.label.color.b = param.label.color.b or 0
+	btn.label.color.a = param.label.color.a or 255
 
 	btn.button = {}
 	btn.button.x = param.button.x or 0
@@ -23,12 +30,21 @@ function Click:newRectangleButton(param)
 	btn.button.width = param.button.width or 400
 	btn.button.height = param.button.height or 100
 	btn.button.radius = math.min(param.button.radius or 0, btn.button.height/2)
+	btn.button.color = {}
+	btn.button.color.r = param.button.color.r or 255
+	btn.button.color.g = param.button.color.g or 255
+	btn.button.color.b = param.button.color.b or 255
+	btn.button.color.a = param.button.color.a or 255
 
-	btn.color = {}
-	btn.color.r = param.color.r or 255
-	btn.color.g = param.color.g or 255
-	btn.color.b = param.color.b or 255
-	btn.color.a = param.color.a or 255
+	btn.border = {}
+	btn.border.width = param.border.width or 2
+	btn.border.color = {}
+	btn.border.color.r = param.border.color.r or 0
+	btn.border.color.g = param.border.color.g or 0
+	btn.border.color.b = param.border.color.b or 0
+	btn.border.color.a = param.border.color.a or 255
+
+	btn.func = param.func 
 
 	table.insert(rectangleButtons, btn)
 end
@@ -36,54 +52,71 @@ end
 function Click:draw()
 	for i=1,#rectangleButtons do
 		local r = rectangleButtons[i]
-		love.graphics.setColor(r.color.r, r.color.g, r.color.b, r.color.a)
+
+		love.graphics.setColor(r.button.color.r, r.button.color.g, r.button.color.b, r.button.color.a)
 		love.graphics.rectangle("fill", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
 
-		love.graphics.setColor(0, 0, 0, 255)
+		if hover[1] and hover[2] == "rectangle" and hover[3] == i then
+			love.graphics.setColor(170, 170, 170, 90)
+			love.graphics.rectangle("fill", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
+		end
+
+		love.graphics.setLineWidth(r.border.width)
+		love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
 		love.graphics.rectangle("line", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
 
-		love.graphics.setColor(0, 0, 0, 255)
+		love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
 		love.graphics.setFont(love.graphics.newFont(r.label.font, r.label.size))
 		love.graphics.printf(r.label.text, r.button.x, r.button.y, r.button.width, "center")
-		--love.graphics.newText()
 	end
 end
 
 function Click:update(dt)
-
-	if inside() then
+	-- MUDA O CURSOR
+	local inside = insideRectangleButton()
+	if inside[1] then
 		love.mouse.setCursor(cursorHand)
+		hover = inside
 	else
 		love.mouse.setCursor()
+		hover = {false, "none", 0}
 	end
-
+	-- Executa a função do botão
+	if love.mouse.isDown(1) and cursorHand == love.mouse.getCursor() and flag then
+		flag = false
+		if inside[2] == "rectangle" then
+			rectangleButtons[inside[3]]:func()
+		end
+	end
+	if not love.mouse.isDown(1) and not flag then
+		flag = true
+	end
 end
 
-function inside()
+function insideRectangleButton()
 	x = love.mouse.getX()
 	y = love.mouse.getY()
 
 	for i=1,#rectangleButtons do
 		local r = rectangleButtons[i].button
-		if x > r.x and x < r.x + r.width 
-			and y > r.y and y < r.y + r.height then
-			if r.radius > 0 
-				and (x > r.x + r.radius and x < r.x + r.width - r.radius) 
-				or (y > r.y + r.radius and y < r.y + r.height - r.radius) 
-				or ((x - (r.x + r.radius))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
-				or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
-				or ((x - (r.x + r.radius))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
-				or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
-				then
-				return true
+		if x > r.x and x < r.x + r.width and y > r.y and y < r.y + r.height then
+			if r.radius > 0 then
+				if (x > r.x + r.radius and x < r.x + r.width - r.radius) 
+					or (y > r.y + r.radius and y < r.y + r.height - r.radius) 
+					or ((x - (r.x + r.radius))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
+					or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
+					or ((x - (r.x + r.radius))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
+					or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius then
+					return {true, "rectangle", i}
+				end
 			else
-				return false
-			end
-			
+				return {true, "rectangle", i}
+			end	
 		else
-			return false
+			return {false}
 		end
 	end
+	return {false}
 end
 
 return Click
--- main.lua
+++ main.lua
@@ -3,10 +3,13 @@ click = require("Click")
 function love.load()
 	love.graphics.setBackgroundColor(170, 170, 170)
 	click:newRectangleButton({
-		ID = "play",
-		label = {text = "PLAY", size = 70, font = "font/FrancoisOne-Regular.ttf"}, 
-		button = {x = 200, y = 200, width = 400, height = 100, radius =  50}, 
-		color = {r = 255, g = 255, b = 255, a = 255}
+		Class = "play",
+		label = {text = "PLAY", size = 70, font = "font/FrancoisOne-Regular.ttf", color = {r = 0, g = 0, b = 0, a = 255}}, 
+		button = {x = 200, y = 200, width = 400, height = 100, radius =  30, color = {r = 255, g = 255, b = 255, a = 255}},
+		border = {width = 2 , color = {r = 0, g = 0, b = 0, a = 255}},
+		func = function ()
+			love.graphics.setBackgroundColor(love.math.random(0, 255), love.math.random(0, 255), love.math.random(0, 255))
+		end
 		})
 end
 
@@ -17,4 +20,5 @@ end
 function love.draw()
 	click:draw()
 end
+
 
