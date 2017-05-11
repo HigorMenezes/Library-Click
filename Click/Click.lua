7d2bbbf0737f0e85e8b8ea6bf9f7b76fae7d6064
higormenezes <higor_menezes96@hotmail.com.br>
2017-05-11 14:32:46 -0400

    Functions set
    Adicionado varias funções SET para poder alterar o design dos botões

--- Click/Click.lua
+++ Click/Click.lua
@@ -9,120 +9,224 @@ local hover = {false, false, 0}
 
 local rectangleButtons = {}
 
+-- Creat a rectangle button
 function Click:newRectangleButton(param)
-	local btn = {}
+	local button = {}
 
-	btn.Class = param.Class or false
+	button.class = param.class or false
 
-	btn.label = {}
+	button.label = {}
 	param.label = param.label or {}
-	btn.label.text = param.label.text or "btn"
-	btn.label.font = param.label.font or fontDefault
-	btn.label.size = param.label.size or 16
-	btn.label.verticalAlign = param.label.verticalAlign or "center"
-	btn.label.horizontalAlign = param.label.horizontalAlign or "center"
-	btn.label.color = {}
+	button.label.text = param.label.text or "button"
+	button.label.font = param.label.font or fontDefault
+	button.label.size = param.label.size or 16
+	button.label.verticalAlign = param.label.verticalAlign or "center"
+	button.label.horizontalAlign = param.label.horizontalAlign or "center"
+	button.label.color = {}
 	param.label.color = param.label.color or {}
-	btn.label.color.r = param.label.color.r or 0
-	btn.label.color.g = param.label.color.g or 0
-	btn.label.color.b = param.label.color.b or 0
-	btn.label.color.a = param.label.color.a or 255
+	button.label.color.r = param.label.color.r or 0
+	button.label.color.g = param.label.color.g or 0
+	button.label.color.b = param.label.color.b or 0
+	button.label.color.a = param.label.color.a or 255
 
-	btn.button = {}
-	param.button = param.button or {}
-	btn.button.x = param.button.x or 0
-	btn.button.y = param.button.y or 0
-	btn.button.width = param.button.width or 400
-	btn.button.height = param.button.height or 100
-	btn.button.radius = math.min(param.button.radius or 0, btn.button.height/2)
-	btn.button.color = {}
-	param.button.color = param.button.color or {}
-	btn.button.color.r = param.button.color.r or 255
-	btn.button.color.g = param.button.color.g or 255
-	btn.button.color.b = param.button.color.b or 255
-	btn.button.color.a = param.button.color.a or 255
+	button.shape = {}
+	param.shape = param.shape or {}
+	button.shape.x = param.shape.x or 0
+	button.shape.y = param.shape.y or 0
+	button.shape.width = param.shape.width or 400
+	button.shape.height = param.shape.height or 100
+	button.shape.radius = math.min(param.shape.radius or 0, button.shape.height/2)
+	button.shape.color = {}
+	param.shape.color = param.shape.color or {}
+	button.shape.color.r = param.shape.color.r or 255
+	button.shape.color.g = param.shape.color.g or 255
+	button.shape.color.b = param.shape.color.b or 255
+	button.shape.color.a = param.shape.color.a or 255
 
-	btn.border = {}
+	button.border = {}
 	param.border = param.border or {}
-	btn.border.width = param.border.width or 0
-	btn.border.color = {}
+	button.border.width = param.border.width or 0
+	button.border.color = {}
 	param.border.color = param.border.color or {}
-	btn.border.color.r = param.border.color.r or 0
-	btn.border.color.g = param.border.color.g or 0
-	btn.border.color.b = param.border.color.b or 0
-	btn.border.color.a = param.border.color.a or 0
+	button.border.color.r = param.border.color.r or 0
+	button.border.color.g = param.border.color.g or 0
+	button.border.color.b = param.border.color.b or 0
+	button.border.color.a = param.border.color.a or 0
 
-	btn.hover = {}
+	button.hover = {}
 	param.hover = param.hover or {}
-	btn.hover.color = {}
+	button.hover.color = {}
 	param.hover.color = param.hover.color or {}
-	btn.hover.color.r = param.hover.color.r or 170
-	btn.hover.color.g = param.hover.color.g or 170
-	btn.hover.color.b = param.hover.color.b or 170
-	btn.hover.color.a = param.hover.color.a or 90
+	button.hover.color.r = param.hover.color.r or 170
+	button.hover.color.g = param.hover.color.g or 170
+	button.hover.color.b = param.hover.color.b or 170
+	button.hover.color.a = param.hover.color.a or 90
 
-	btn.shadow = {}
+	button.shadow = {}
 	param.shadow = param.shadow or {}
-	btn.shadow.top = param.shadow.top or 0
-	btn.shadow.right = param.shadow.right or 0
-	btn.shadow.down = param.shadow.down or 0
-	btn.shadow.left = param.shadow.left or 0
-	btn.shadow.color = {}
+	button.shadow.top = param.shadow.top or 0
+	button.shadow.right = param.shadow.right or 0
+	button.shadow.down = param.shadow.down or 0
+	button.shadow.left = param.shadow.left or 0
+	button.shadow.color = {}
 	param.shadow.color = param.shadow.color or {}
-	btn.shadow.color.r = param.shadow.color.r or 170
-	btn.shadow.color.g = param.shadow.color.g or 170
-	btn.shadow.color.b = param.shadow.color.b or 170
-	btn.shadow.color.a = param.shadow.color.a or 90
+	button.shadow.color.r = param.shadow.color.r or 170
+	button.shadow.color.g = param.shadow.color.g or 170
+	button.shadow.color.b = param.shadow.color.b or 170
+	button.shadow.color.a = param.shadow.color.a or 90
 
-	btn.func = param.func or function() end
+	button.func = param.func or function() end
 
-	table.insert(rectangleButtons, btn)
+	table.insert(rectangleButtons, button)
 end
 
+function Click:setLabelRectangleButton(class, param)
+	label = param.label or {}
+	label.color = label.color or {}
+
+	for i=1,#rectangleButtons do
+		if rectangleButtons[i].class == class then
+			local r = rectangleButtons[i].label
+			r.text = label.text or r.text
+			r.size = label.size or r.size
+			r.font = label.font or r.font
+			r.verticalAlign = label.verticalAlign or r.verticalAlign
+			r.horizontalAlign = label.horizontalAlign or r.horizontalAlign
+			r.color.r = label.color.r or r.color.r
+			r.color.g = label.color.g or r.color.g
+			r.color.b = label.color.b or r.color.b
+			r.color.a = label.color.a or r.color.a
+		end
+	end
+end
+
+function Click:setShapeRectangleButton(class, param)
+	shape = param.shape or {}
+	shape.color = shape.color or {}
+
+	for i=1,#rectangleButtons do
+		if rectangleButtons[i].class == class then
+			local r = rectangleButtons[i].shape
+			r.x = shape.x or r.x
+			r.y = shape.y or r.y
+			r.width = shape.width or r.width
+			r.height = shape.height or r.height
+			r.radius = shape.radius or r.radius
+			r.color.r = shape.color.r or r.color.r
+			r.color.g = shape.color.g or r.color.g
+			r.color.b = shape.color.b or r.color.b
+			r.color.a = shape.color.a or r.color.a
+		end
+	end
+end
+
+function Click:setBorderRectangleButton(class, param)
+	border = param.border or {}
+	border.color = border.color or {}
+
+	for i=1,#rectangleButtons do
+		if rectangleButtons[i].class == class then
+			local r = rectangleButtons[i].border
+			r.width = border.width or r.width
+			r.color.r = border.color.r or r.color.r
+			r.color.g = border.color.g or r.color.g
+			r.color.b = border.color.b or r.color.b
+			r.color.a = border.color.a or r.color.a
+		end
+	end
+end
+
+function Click:setHoverRectangleButton(class, param)
+	hover = param.hover or {}
+	hover.color = hover.color or {}
+
+	for i=1,#rectangleButtons do
+		if rectangleButtons[i].class == class then
+			local r = rectangleButtons[i].hover
+			r.color.r = hover.color.r or r.color.r
+			r.color.g = hover.color.g or r.color.g
+			r.color.b = hover.color.b or r.color.b
+			r.color.a = hover.color.a or r.color.a
+		end
+	end
+end
+
+function Click:setShadowRectangleButton(class, param)
+	shadow = param.shadow or {}
+	shadow.color = shadow.color or {}
+
+	for i=1,#rectangleButtons do
+		if rectangleButtons[i].class == class then
+			local r = rectangleButtons[i].shadow
+			r.top = shadow.top or r.top
+			r.down = shadow.down or r.down
+			r.right = shadow.right or r.right
+			r.left = shadow.left or r.left
+			r.color.r = shadow.color.r or r.color.r
+			r.color.g = shadow.color.g or r.color.g
+			r.color.b = shadow.color.b or r.color.b
+			r.color.a = shadow.color.a or r.color.a
+		end
+	end
+end
+
+function Click:setFuncRectangleButton(class, param)
+	func = param.func or function() end
+
+	for i=1,#rectangleButtons do
+		local r = rectangleButtons[i]
+		if r.class == class then
+			r.func = func or r.func
+		end
+	end
+end
+
+
+-- CALL BACKS -------------------------------------
 function Click:draw()
 	for i=1,#rectangleButtons do
 		local r = rectangleButtons[i]
 		--shadow
 		love.graphics.setColor(r.shadow.color.r, r.shadow.color.g, r.shadow.color.b, r.shadow.color.a)
-		love.graphics.rectangle("fill", (r.button.x - r.shadow.left), (r.button.y - r.shadow.top), 
-			(r.button.width + r.shadow.left + r.shadow.right), (r.button.height + r.shadow.top + r.shadow.down), r.button.radius)
+		love.graphics.rectangle("fill", (r.shape.x - r.shadow.left), (r.shape.y - r.shadow.top), 
+			(r.shape.width + r.shadow.left + r.shadow.right), (r.shape.height + r.shadow.top + r.shadow.down), r.shape.radius)
 		-- Button
-		love.graphics.setColor(r.button.color.r, r.button.color.g, r.button.color.b, r.button.color.a)
-		love.graphics.rectangle("fill", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
+		love.graphics.setColor(r.shape.color.r, r.shape.color.g, r.shape.color.b, r.shape.color.a)
+		love.graphics.rectangle("fill", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
 		-- Hover
 		if hover[1] and hover[2] == "rectangle" and hover[3] == i then
 			love.graphics.setColor(r.hover.color.r, r.hover.color.g, r.hover.color.b, r.hover.color.a)
-			love.graphics.rectangle("fill", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
+			love.graphics.rectangle("fill", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
 		end
 		--Border
 		love.graphics.setLineWidth(r.border.width)
 		love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
-		love.graphics.rectangle("line", r.button.x, r.button.y, r.button.width, r.button.height, r.button.radius)
+		love.graphics.rectangle("line", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
 		--Label
 		love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
 		local text = love.graphics.newText(love.graphics.newFont(r.label.font, r.label.size), r.label.text)
 		local x, y = 0, 0
 
 		if r.label.horizontalAlign == "left" then
-			x = r.button.x + text:getWidth()/2 + r.border.width
+			x = r.shape.x + text:getWidth()/2 + r.border.width
 		elseif r.label.horizontalAlign == "center" then
-			x = r.button.x + r.button.width/2
+			x = r.shape.x + r.shape.width/2
 		else
-			x = r.button.x + r.button.width - text:getWidth()/2 - r.border.width
+			x = r.shape.x + r.shape.width - text:getWidth()/2 - r.border.width
 		end
 
 		if r.label.verticalAlign == "top" then
-			y = r.button.y + r.button.height - text:getHeight()/2 - r.border.width
+			y = r.shape.y + text:getHeight()/2 + (r.border.width/2)
 		elseif r.label.verticalAlign == "center" then
-			y = r.button.y + r.button.height/2
+			y = r.shape.y + r.shape.height/2
 		else
-			y = r.button.y + text:getHeight()/2 + r.border.width
+			y = r.shape.y + r.shape.height - text:getHeight()/2 - (r.border.width/2)
 		end
 
 		love.graphics.draw(text, x, y, 0, 1, 1, text:getWidth()/2, text:getHeight()/2)
 
 		--love.graphics.setFont(love.graphics.newFont(r.label.font, r.label.size))
-		--love.graphics.printf(r.label.text, r.button.x, r.button.y, r.button.width, "center")
+		--love.graphics.printf(r.label.text, r.shape.x, r.shape.y, r.shape.width, "center")
 	end
 end
 
@@ -153,7 +257,7 @@ function insideButton()
 	y = love.mouse.getY()
 
 	for i=1,#rectangleButtons do
-		local r = rectangleButtons[i].button
+		local r = rectangleButtons[i].shape
 		if x > r.x and x < r.x + r.width and y > r.y and y < r.y + r.height then
 			if r.radius > 0 then
 				if (x > r.x + r.radius and x < r.x + r.width - r.radius) 
@@ -171,5 +275,6 @@ function insideButton()
 	end
 	return {false}
 end
+---------------------------------------------------------------------------
 
 return Click
--- main.lua
+++ main.lua
@@ -3,30 +3,30 @@ click = require("Click")
 function love.load()
 	love.graphics.setBackgroundColor(200, 200, 200)
 	click:newRectangleButton({
-		Class = "play",
-		label = {text = "PLAY", size = 80, font = "font/FrancoisOne-Regular.ttf", verticalAlign = "down", horizontalAlign = "left", 
-			color = {r = 0, g = 0, b = 0, a = 255}}, 
-		button = {x = 200, y = 200, width = 400, height = 100, radius =  15, color = {r = 255, g = 255, b = 255, a = 255}},
-		border = {width = 2, color = {r = 0, g = 0, b = 0, a = 255}},
-		hover = {color = {r = 170, g = 170, b = 170, a = 90}},
-		shadow = {top = 10, right = 0, down = 0, left = 0, color = {r = 0, g = 0, b = 170, a = 255}},
+		class = "menu",
+		label = {text = "PLAY"}, 
+		shape = {x = 200, y = 200, width = 400, height = 100},
 		func = function ()
-			love.graphics.setBackgroundColor(70, 70, 70)
+			love.graphics.setBackgroundColor(180, 180, 180)
 		end
 		})
 
 	click:newRectangleButton({
-		Class = "option",
-		label = {text = "OPTION", size = 70, font = "font/FrancoisOne-Regular.ttf", verticalAlign = "top", horizontalAlign = "right", 
-			color = {r = 255, g = 255, b = 255, a = 255}}, 
-		button = {x = 200, y = 320, width = 400, height = 100, radius =  50, color = {r = 0, g = 0, b = 0, a = 255}},
-		border = {width = 10, color = {r = 255, g = 255, b = 255, a = 255}},
-		hover = {color = {r = 200, g = 200, b = 200, a = 90}},
-		shadow = {top = 0, right = 0, down = 10, left = 0, color = {r = 170, g = 0, b = 0, a = 255}},
+		class = "menu",
+		label = {text = "OPTION"}, 
+		shape = {x = 200, y = 320, width = 400, height = 100},
 		func = function ()
 			love.graphics.setBackgroundColor(140, 140, 140)
 		end
 		})
+
+	click:setLabelRectangleButton("menu", {label={size=50,font = "font/FrancoisOne-Regular.ttf",color={r=0,g=0,b=0,a=255}}})
+	click:setShapeRectangleButton("menu", {shape={radius=15,color={r=255,g=255,b=255,a=255}}})
+	click:setBorderRectangleButton("menu", {border={width = 2, color = {r = 0, g = 0, b = 0, a = 255}}})
+	click:setHoverRectangleButton("menu", {hover={color = {r = 120, g = 120, b = 120, a = 90}}})
+	click:setShadowRectangleButton("menu", {shadow={top=0,right=5,down=5,left=0,color={r=70,g=70,b=70,a=40}}})
+
+
 end
 
 function love.update(dt)
