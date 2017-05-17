665ada3fc004ccfb54c02f1c9c4fa2619cf0a11d
higormenezes <higor_menezes96@hotmail.com.br>
2017-05-17 12:56:19 -0400

    Panel
    Inserção de painel no Click

--- Click/Click.lua
+++ Click/Click.lua
@@ -33,6 +33,14 @@ function Click:newArcButton(param)
 	return button
 end
 
+function Click:newPanel(param)
+	param = param or {}
+	local panel = createPanel(param)
+
+	table.insert(panels, panel)
+	return panel
+end
+
 function Click:remove(button)
 	for i=1,#buttons do
 		if buttons[i] == button then
@@ -58,6 +66,11 @@ function Click:setVisibleByClass(class, visible)
 	for i=1,#buttons do
 		if buttons[i].class == class then
 			buttons[i].visible = visible
+		end
+	end
+	for i=1,#panels do
+		if panels[i].class == class then
+			panels[i].visible = visible
 		end
 	end
 end
@@ -170,10 +183,11 @@ end
 -- CALL BACKS -------------------------------------
 function Click:draw()
 	
-	for i=1,#buttons do
-		--RectangleButtons
-		if buttons[i].visible and buttons[i].buttonType == "rectangle" then
-			local r = buttons[i]
+	drawButtons(buttons)
+	for i=1,#panels do
+		if panels[i].visible then
+			local r = panels[i]
+
 			--shadow
 			love.graphics.setColor(r.shadow.color.r, r.shadow.color.g, r.shadow.color.b, r.shadow.color.a)
 			love.graphics.rectangle("fill", (r.shape.x - r.shadow.left), (r.shape.y - r.shadow.top), 
@@ -181,82 +195,17 @@ function Click:draw()
 			-- Shape
 			love.graphics.setColor(r.shape.color.r, r.shape.color.g, r.shape.color.b, r.shape.color.a)
 			love.graphics.rectangle("fill", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
-			-- Hover
-			if hover[1] and hover[2] == "rectangle" and hover[3] == i then
-				love.graphics.setColor(r.hover.color.r, r.hover.color.g, r.hover.color.b, r.hover.color.a)
-				love.graphics.rectangle("fill", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
-			end
 			--Border
 			love.graphics.setLineWidth(r.border.width)
 			love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
 			love.graphics.rectangle("line", r.shape.x, r.shape.y, r.shape.width, r.shape.height, r.shape.radius)
 			--Label
 			love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
-			local text = love.graphics.newText(r.label.font, r.label.text)
-			local x, y = 0, 0
+			love.graphics.setFont(r.label.font)
+			love.graphics.printf(r.label.text, r.label.x + r.shape.x, r.label.y + r.shape.y, r.label.limit, r.label.align)
 
-			if r.label.horizontalAlign == "left" then
-				x = r.shape.x + text:getWidth()/2 + r.border.width
-			elseif r.label.horizontalAlign == "center" then
-				x = r.shape.x + r.shape.width/2
-			else
-				x = r.shape.x + r.shape.width - text:getWidth()/2 - r.border.width
-			end
+			drawButtons(r.buttons, r.shape.x, r.shape.y)
 
-			if r.label.verticalAlign == "top" then
-				y = r.shape.y + text:getHeight()/2 + (r.border.width/2)
-			elseif r.label.verticalAlign == "center" then
-				y = r.shape.y + r.shape.height/2
-			else
-				y = r.shape.y + r.shape.height - text:getHeight()/2 - (r.border.width/2)
-			end
-
-			love.graphics.draw(text, x, y, 0, 1, 1, text:getWidth()/2, text:getHeight()/2)
-		--Arc button
-		elseif buttons[i].visible and buttons[i].buttonType == "arc" then
-			local r = buttons[i]
-
-			--shadow
-			love.graphics.setColor(r.shadow.color.r, r.shadow.color.g, r.shadow.color.b, r.shadow.color.a)
-			love.graphics.setLineWidth(r.shape.width + math.abs(r.shadow.verticalDeslocation))
-			love.graphics.arc("line", "open", r.shape.x, r.shape.y, r.shape.radius + r.shadow.verticalDeslocation, 
-				r.shape.startAng - r.shadow.left, r.shape.finalAng + r.shadow.right)
-
-			--Shape
-			love.graphics.setColor(r.shape.color.r, r.shape.color.g, r.shape.color.b, r.shape.color.a)
-			love.graphics.setLineWidth(r.shape.width)
-			love.graphics.arc("line", "open", r.shape.x, r.shape.y, r.shape.radius, r.shape.startAng, r.shape.finalAng)	
-
-			--Hover
-			if hover[1] and hover[2] == "arc" and hover[3] == i then
-				love.graphics.setColor(r.hover.color.r, r.hover.color.g, r.hover.color.b, r.hover.color.a)
-				love.graphics.arc("line", "open", r.shape.x, r.shape.y, r.shape.radius, r.shape.startAng, r.shape.finalAng)	
-			end
-
-			--border
-			love.graphics.setLineWidth(r.border.width)
-			love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
-			love.graphics.arc("line", "open", r.shape.x, r.shape.y, r.shape.radius + r.shape.width/2, r.shape.startAng, r.shape.finalAng)	
-			love.graphics.arc("line", "open", r.shape.x, r.shape.y, r.shape.radius - r.shape.width/2, r.shape.startAng, r.shape.finalAng)
-			love.graphics.line((r.shape.x + math.cos(r.shape.startAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2)) , 
-				(r.shape.y + math.sin(r.shape.startAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2)) ,
-				r.shape.x + math.cos(r.shape.startAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2), 
-				r.shape.y + math.sin(r.shape.startAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2))
-			love.graphics.line(r.shape.x + math.cos(r.shape.finalAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2), 
-				r.shape.y + math.sin(r.shape.finalAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2),
-				r.shape.x + math.cos(r.shape.finalAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2), 
-				r.shape.y + math.sin(r.shape.finalAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2))
-
-			--Label
-			love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
-			local center = (r.shape.startAng + r.shape.finalAng)/2
-			local stringAng = r.label.space * string.len(r.label.text)
-			for i=1,#r.label.tabText do
-				local text = love.graphics.newText(r.label.font, r.label.tabText[i])
-				love.graphics.draw(text, r.shape.x+(math.cos((center - stringAng/2 + r.label.space/2)+r.label.space*(i-1))*r.shape.radius), 
-					r.shape.y+(math.sin((center - stringAng/2 + r.label.space/2)+r.label.space*(i-1))*r.shape.radius), 
-					math.rad(90)+(center - stringAng/2 + r.label.space/2)+r.label.space*(i-1), 1, 1, text:getWidth()/2, text:getHeight()/2)
-			end
 		end
 	end
 
@@ -264,10 +213,18 @@ end
 
 function Click:update(dt)
 	-- MUDA O CURSOR
-	local inside = insideButton()
+	local inside
+	inside = insideButton(buttons)
+
+	for i=1,#panels do
+		if panels[i].visible then
+			inside = insideButton(panels[i].buttons, panels[i].shape.x, panels[i].shape.y, i)
+		end
+	end
+
 	if inside[1] then
 		love.mouse.setCursor(cursorHand)
-		hover = inside
+		hover = {inside[1], inside[2], inside[3]}
 	else
 		love.mouse.setCursor()
 		hover = {false, false, 0}
@@ -275,9 +232,9 @@ function Click:update(dt)
 	-- Executa a função do botão
 	if love.mouse.isDown(1) and cursorHand == love.mouse.getCursor() and flag then
 		flag = false
-		if inside[2] == "rectangle" then
-			buttons[inside[3]]:func()
-		elseif inside[2] == "arc" then
+		if inside[4] > 0 then
+			panels[inside[4]].buttons[inside[3]]:func()
+		else
 			buttons[inside[3]]:func()
 		end
 	end
@@ -366,60 +323,232 @@ function createButton(param)
 	return button
 end
 
-function insideButton()
-	x = love.mouse.getX()
-	y = love.mouse.getY()
+function createPanel(param)
+	local panel = {}
 
-	
-	for i=1,#buttons do
+	param = param or {}
+
+	panel.class = param.class or false
+	panel.visible = param.visible or false
+
+	panel.shape = {}
+	param.shape = param.shape or {}
+	panel.shape.x = param.shape.x or 0
+	panel.shape.y = param.shape.y or 0
+	panel.shape.width = param.shape.width or 40
+	panel.shape.height = param.shape.height or 100
+	panel.shape.radius = math.min(param.shape.radius, panel.shape.height/2)
+	panel.shape.color = {}
+	param.shape.color = param.shape.color or {}
+	panel.shape.color.r = param.shape.color.r or 255
+	panel.shape.color.g = param.shape.color.g or 255
+	panel.shape.color.b = param.shape.color.b or 255
+	panel.shape.color.a = param.shape.color.a or 255
+
+	panel.label = {}
+	param.label = param.label or {}
+	panel.label.text = param.label.text or "panel"
+	panel.label.font = love.graphics.newFont(param.label.file or fileDefault, param.label.size or 16)
+	panel.label.x = param.label.x or 0
+	panel.label.y = param.label.y or 0
+	panel.label.limit = param.label.limit or panel.shape.width
+	panel.label.align = param.label.align or "center"
+	panel.label.color = {}
+	param.label.color = param.label.color or {}
+	panel.label.color.r = param.label.color.r or 0
+	panel.label.color.g = param.label.color.g or 0
+	panel.label.color.b = param.label.color.b or 0
+	panel.label.color.a = param.label.color.a or 255
+
+	panel.border = {}
+	param.border = param.border or {}
+	panel.border.width = param.border.width or 0
+	panel.border.color = {}
+	param.border.color = param.border.color or {}
+	panel.border.color.r = param.border.color.r or 0
+	panel.border.color.g = param.border.color.g or 0
+	panel.border.color.b = param.border.color.b or 0
+	panel.border.color.a = param.border.color.a or 0
+
+	panel.shadow = {}
+	param.shadow = param.shadow or {}
+	panel.shadow.top = param.shadow.top or 0
+	panel.shadow.right = param.shadow.right or 0
+	panel.shadow.down = param.shadow.down or 0
+	panel.shadow.left = param.shadow.left or 0
+	panel.shadow.color = {}
+	param.shadow.color = param.shadow.color or {}
+	panel.shadow.color.r = param.shadow.color.r or 170
+	panel.shadow.color.g = param.shadow.color.g or 170
+	panel.shadow.color.b = param.shadow.color.b or 170
+	panel.shadow.color.a = param.shadow.color.a or 90
+
+	panel.buttons = {}
+	panel.buttons = param.buttons or {}
+	for i=1,#panel.buttons do
+		for j=1,#buttons do
+			if panel.buttons[i] == buttons[j] then
+				table.remove(buttons, j)
+				break
+			end
+		end
+	end
+
+	return panel
+end
+
+function insideButton(btns, originX, originY, panel)
+	originX = originX or 0
+	originY = originY or 0
+	panel = panel or 0
+	local x = love.mouse.getX()
+	local y = love.mouse.getY()
+
+	for i=1,#btns do
 		-- Inside in rectangle
-		if buttons[i].clickable and buttons[i].visible and buttons[i].buttonType == "rectangle" then
-			local r = buttons[i].shape
-			if x > r.x and x < r.x + r.width and y > r.y and y < r.y + r.height then
+		if btns[i].clickable and btns[i].visible and btns[i].clickable and btns[i].buttonType == "rectangle" then
+			local r = btns[i].shape
+			if x > (r.x + originX) and x < (r.x + originX) + r.width and y > (r.y + originY) and y < (r.y + originY) + r.height then
 				if r.radius > 0 then
-					if (x > r.x + r.radius and x < r.x + r.width - r.radius) 
-						or (y > r.y + r.radius and y < r.y + r.height - r.radius) 
-						or ((x - (r.x + r.radius))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
-						or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.radius))^2)^(1/2) < r.radius 
-						or ((x - (r.x + r.radius))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius 
-						or ((x - (r.x - r.radius + r.width))^2 + (y - (r.y + r.height - r.radius))^2)^(1/2) < r.radius then
-						return {true, "rectangle", i}
+					if (x > (r.x + originX) + r.radius and x < (r.x + originX) + r.width - r.radius) 
+						or (y > (r.y + originY) + r.radius and y < (r.y + originY) + r.height - r.radius) 
+						or ((x - ((r.x + originX) + r.radius))^2 + (y - ((r.y + originY) + r.radius))^2)^(1/2) < r.radius 
+						or ((x - ((r.x + originX) - r.radius + r.width))^2 + (y - ((r.y + originY) + r.radius))^2)^(1/2) < r.radius 
+						or ((x - ((r.x + originX) + r.radius))^2 + (y - ((r.y + originY) + r.height - r.radius))^2)^(1/2) < r.radius 
+						or ((x - ((r.x + originX) - r.radius + r.width))^2 + (y - ((r.y + originY) + r.height - r.radius))^2)^(1/2) < r.radius then
+						return {true, "rectangle", i, panel}
 					end
 				else
-					return {true, "rectangle", i}
+					return {true, "rectangle", i, panel}
 				end	
 			end
 		--inside arc
-		elseif buttons[i].clickable and buttons[i].visible and buttons[i].buttonType == "arc" then
-			local r = buttons[i].shape
+		elseif btns[i].clickable and btns[i].visible and btns[i].clickable and btns[i].buttonType == "arc" then
+			local r = btns[i].shape
 			
 			local radiusMax = r.radius + r.width/2
 			local radiusMin = r.radius - r.width/2
-			local distance = ((x - r.x)^2 + (y - r.y)^2)^(1/2)
+			local distance = ((x - (r.x + originX))^2 + (y - (r.y + originY))^2)^(1/2)
 
 			local angMax
-			if y - r.y > 0 then
-				angMax = math.acos((x-r.x)/distance)
+			if y - (r.y + originY) > 0 then
+				angMax = math.acos((x-(r.x + originX))/distance)
 			else
-				angMax = math.acos((r.x-x)/distance) + math.rad(180)
+				angMax = math.acos(((r.x + originX)-x)/distance) + math.rad(180)
 			end
 			
 			local angMin 
-			if y - r.y > 0 then
-				angMin = -math.acos((r.x-x)/distance) - math.rad(180)
+			if y - (r.y + originY) > 0 then
+				angMin = -math.acos(((r.x + originX)-x)/distance) - math.rad(180)
 			else
-				angMin = -math.acos((x-r.x)/distance)
+				angMin = -math.acos((x-(r.x + originX))/distance)
 			end
 
 			if distance < radiusMax and distance > radiusMin 
 				and (((angMax > r.startAng and angMax < r.finalAng) or (angMax < r.startAng and angMax > r.finalAng)) 
 				or ((angMin < r.startAng and angMin > r.finalAng) or (angMin > r.startAng and angMin < r.finalAng))) then
-				return {true, "arc", i}
+				return {true, "arc", i, panel}
 			end
 		end
 	end
 
 	return {false}
+end
+
+function drawButtons(btns, originX, originY)
+	originX = originX or 0
+	originY = originY or 0
+	for i=1,#btns do
+		--Rectanglebtns
+		if btns[i].visible and btns[i].buttonType == "rectangle" then
+			local r = btns[i]
+			--shadow
+			love.graphics.setColor(r.shadow.color.r, r.shadow.color.g, r.shadow.color.b, r.shadow.color.a)
+			love.graphics.rectangle("fill", (r.shape.x - r.shadow.left + originX), (r.shape.y - r.shadow.top + originY), 
+				(r.shape.width + r.shadow.left + r.shadow.right), (r.shape.height + r.shadow.top + r.shadow.down), r.shape.radius)
+			-- Shape
+			love.graphics.setColor(r.shape.color.r, r.shape.color.g, r.shape.color.b, r.shape.color.a)
+			love.graphics.rectangle("fill", r.shape.x + originX, r.shape.y + originY, r.shape.width, r.shape.height, r.shape.radius)
+			-- Hover
+			if hover[1] and hover[2] == "rectangle" and hover[3] == i then
+				love.graphics.setColor(r.hover.color.r, r.hover.color.g, r.hover.color.b, r.hover.color.a)
+				love.graphics.rectangle("fill", r.shape.x + originX, r.shape.y + originY, r.shape.width, r.shape.height, r.shape.radius)
+			end
+			--Border
+			love.graphics.setLineWidth(r.border.width)
+			love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
+			love.graphics.rectangle("line", r.shape.x + originX, r.shape.y + originY, r.shape.width, r.shape.height, r.shape.radius)
+			--Label
+			love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
+			local text = love.graphics.newText(r.label.font, r.label.text)
+			local x, y = 0, 0
+
+			if r.label.horizontalAlign == "left" then
+				x = r.shape.x + text:getWidth()/2 + r.border.width
+			elseif r.label.horizontalAlign == "center" then
+				x = r.shape.x + r.shape.width/2
+			else
+				x = r.shape.x + r.shape.width - text:getWidth()/2 - r.border.width
+			end
+
+			if r.label.verticalAlign == "top" then
+				y = r.shape.y + text:getHeight()/2 + (r.border.width/2)
+			elseif r.label.verticalAlign == "center" then
+				y = r.shape.y + r.shape.height/2
+			else
+				y = r.shape.y + r.shape.height - text:getHeight()/2 - (r.border.width/2)
+			end
+
+			love.graphics.draw(text, x + originX, y + originY, 0, 1, 1, text:getWidth()/2, text:getHeight()/2)
+		--Arc button
+		elseif btns[i].visible and btns[i].buttonType == "arc" then
+			local r = btns[i]
+
+			--shadow
+			love.graphics.setColor(r.shadow.color.r, r.shadow.color.g, r.shadow.color.b, r.shadow.color.a)
+			love.graphics.setLineWidth(r.shape.width + math.abs(r.shadow.verticalDeslocation))
+			love.graphics.arc("line", "open", r.shape.x + originX, r.shape.y + originY, r.shape.radius + r.shadow.verticalDeslocation, 
+				r.shape.startAng - r.shadow.left, r.shape.finalAng + r.shadow.right)
+
+			--Shape
+			love.graphics.setColor(r.shape.color.r, r.shape.color.g, r.shape.color.b, r.shape.color.a)
+			love.graphics.setLineWidth(r.shape.width)
+			love.graphics.arc("line", "open", r.shape.x + originX, r.shape.y + originY, r.shape.radius, r.shape.startAng, r.shape.finalAng)	
+
+			--Hover
+			if hover[1] and hover[2] == "arc" and hover[3] == i then
+				love.graphics.setColor(r.hover.color.r, r.hover.color.g, r.hover.color.b, r.hover.color.a)
+				love.graphics.arc("line", "open", r.shape.x + originX, r.shape.y + originY, r.shape.radius, r.shape.startAng, r.shape.finalAng)	
+			end
+
+			--border
+			love.graphics.setLineWidth(r.border.width)
+			love.graphics.setColor(r.border.color.r, r.border.color.g, r.border.color.b, r.border.color.a)
+			love.graphics.arc("line", "open", r.shape.x + originX, r.shape.y + originY, 
+				r.shape.radius + r.shape.width/2, r.shape.startAng, r.shape.finalAng)	
+			love.graphics.arc("line", "open", r.shape.x + originX, r.shape.y + originY, 
+				r.shape.radius - r.shape.width/2, r.shape.startAng, r.shape.finalAng)
+			love.graphics.line((r.shape.x + math.cos(r.shape.startAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2)) + originX, 
+				(r.shape.y + math.sin(r.shape.startAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2)) + originY,
+				r.shape.x + math.cos(r.shape.startAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2) + originX, 
+				r.shape.y + math.sin(r.shape.startAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2) + originY)
+			love.graphics.line(r.shape.x + math.cos(r.shape.finalAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2) + originX, 
+				r.shape.y + math.sin(r.shape.finalAng)*(r.shape.radius - r.shape.width/2 - r.border.width/2) + originY,
+				r.shape.x + math.cos(r.shape.finalAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2) + originX, 
+				r.shape.y + math.sin(r.shape.finalAng)*(r.shape.radius + r.shape.width/2 + r.border.width/2) + originY)
+
+			--Label
+			love.graphics.setColor(r.label.color.r, r.label.color.g, r.label.color.b, r.label.color.a)
+			local center = (r.shape.startAng + r.shape.finalAng)/2
+			local stringAng = r.label.space * string.len(r.label.text)
+			for i=1,#r.label.tabText do
+				local text = love.graphics.newText(r.label.font, r.label.tabText[i])
+				love.graphics.draw(text, r.shape.x+(math.cos((center - stringAng/2 + r.label.space/2)+r.label.space*(i-1))*r.shape.radius) + originX, 
+					r.shape.y+(math.sin((center - stringAng/2 + r.label.space/2)+r.label.space*(i-1))*r.shape.radius) + originY, 
+					math.rad(90)+(center - stringAng/2 + r.label.space/2)+r.label.space*(i-1), 1, 1, text:getWidth()/2, text:getHeight()/2)
+			end
+		end
+	end
 end
 ---------------------------------------------------------------------------
 
